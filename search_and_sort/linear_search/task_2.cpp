fbf3a5c2545eeaccbbf020c78409ebb3f71f04dd
KULINININ <aleks680026032001@gmail.com>
2020-08-11 17:49:43 +0500

    Add task_2

---
+++ search_and_sort/linear_search/task_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[1000];
+	int x, n;
+
+	string regul;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	cin >> x;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == x)
+		{
+			regul = "YES";
+		}
+		else if (regul != "YES")
+		{
+			regul = "NO";
+		}
+	}
+
+	cout << regul;
+}
